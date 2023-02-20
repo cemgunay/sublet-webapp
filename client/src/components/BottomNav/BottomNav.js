f88fd9b543c142574f7b8263493baefb46ba1d7b
Cem Gunay <cgunay@uwaterloo.ca>
2023-02-20 18:49:36 -0500

    Created bottom navigation

--- client/src/components/BottomNav/BottomNav.js
+++ client/src/components/BottomNav/BottomNav.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import classes from './BottomNav.module.css'
 
 function BottomNav() {
   return (
-    <div>Main Navigation</div>
+    <div className={classes.wrapper}>
+      <footer className={classes.container}>
+      <nav>
+        <Link to='/'>Explore</Link>
+      </nav>
+      <nav>
+        <Link to='/signup'>Profile</Link>
+      </nav>
+      </footer>
+    </div>
   )
 }
 
