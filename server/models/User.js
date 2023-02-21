7539805d25e559b85da3feff390fc2c084dc37bb
NinoS99 <spasiknino23@gmail.com>
2023-02-20 20:37:16 -0500

    mongo db setup, user schema creation, user register API creation

---
+++ server/models/User.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    firstName:{
+        type: String,
+        require: true,
+    },
+    lastName:{
+        type: String,
+        require: true,
+    },
+    email:{
+        type: String,
+        require: true,
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6,
+    },
+    phoneNumber:{
+        type:String,
+        required:true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+        trim: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "",
+    },
+    about: {
+        type: String,
+        default: "",
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    idVerified: {
+        type: Boolean,
+    },
+    occupation: {
+        type: String,
+    },
+    drinking: {
+        type: Boolean,
+    },
+    smoking: {
+        type: Boolean,
+    },
+    noiseLevel: {
+        type: Number,
+        min: 1,
+        max: 100,
+    },
+    instagramUrl: {
+        type: String,
+    },
+    facebookUrl: {
+        type: String,
+    },
+    tikTokUrl: {
+        type: String,
+    },
+
+}, {timestamps:true}
+);
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
