22b3192f23514e7df017893b9b40db1a31f64137
NinoS99 <spasiknino23@gmail.com>
2023-02-26 20:45:52 -0500

    changing login responses to 200 even if credentials are WRONG

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -33,10 +33,10 @@ router.post("/register", async(req,res)=>{
 router.post("/login", async (req,res) => {
     try{
         const user = await User.findOne({ email:req.body.email });
-        !user && res.status(404).send("User not found!");
+        !user && res.status(200).send("User not found!");
 
         const validPassword = await bcrypt.compare(req.body.password, user.password)
-        !validPassword && res.status(400).json("Wrong password");
+        !validPassword && res.status(200).json("Wrong password");
 
         res.status(200).json(user);
     } catch(err){
